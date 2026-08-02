8f1169628a8e99931764ae87eda62a726a6b67d9
5700586-debug <5700586@bk.ru>
2026-08-02 15:33:04 +0000

    Warehouse AI created

---
+++ warehouse_ai.py
@@ -0,0 +1,17 @@
+"""
+Fokus AI
+Warehouse AI Agent
+
+Vazifalari:
+- Ombor qoldig'ini nazorat qilish
+- Haftalik hisobotlarni solishtirish
+- Tannarx o'zgarishini aniqlash
+- Ustama kamayishini nazorat qilish
+- Noto'g'ri prixod haqida ogohlantirish
+- Farqlarni rahbarga hisobot qilish
+"""
+
+class WarehouseAI:
+
+    def analyze(self):
+        print("Warehouse AI ishga tushdi...")
